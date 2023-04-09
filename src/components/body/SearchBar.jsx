84b26f85e258e42baa479d38b53a6fdd5487e715
Mardie328 <tolayinka328@gmail.com>
2023-04-09 18:14:15 +0100

    useState functionality added

--- src/components/body/SearchBar.jsx
+++ src/components/body/SearchBar.jsx
@@ -1,14 +1,28 @@
 import "./SearchBar.css";
+import { useState } from "react";
 import searchBarIcon from "../../Images/search-bar.svg";
 import microphoneIcon from "../../Images/microphone-icon.png";
 import googleLensIcon from "../../Images/google-lens.png";
 
 const SearchBar = () => {
+  const [inputValue, setInputValue] = useState("");
+  function handleEnterKeyPress(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      setInputValue(event.target.value);
+      event.target.value = "";
+    }
+  }
+
   return (
     <div className="top-container">
       <img src={searchBarIcon} className="search-icon" alt="search bar icon" />
       <form>
-        <input type="text" className="input"></input>
+        <input
+          type="text"
+          className="input"
+          onKeyDown={handleEnterKeyPress}
+        ></input>
       </form>
       <img
         src={microphoneIcon}
@@ -21,6 +35,12 @@ const SearchBar = () => {
         className="googleLens-icon"
         alt="search image icon"
       />
+
+      {inputValue && (
+        <div className="newInputContainer">
+          <p className="newInput">{inputValue}</p>
+        </div>
+      )}
     </div>
   );
 };
